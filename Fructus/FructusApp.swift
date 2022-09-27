4d07edc3eccc92e6b03e7ea4bd80058938642dcb
Baris Ciftci <barishciftci@gmail.com>
2022-09-27 19:37:07 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Baris Ciftci on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
