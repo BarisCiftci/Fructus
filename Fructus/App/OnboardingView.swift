58259859ca455465b3f70d7f06e53367371a57c8
Baris Ciftci <barishciftci@gmail.com>
2022-10-04 22:42:17 +0200

    create data model and fetch data using Swift

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -10,15 +10,17 @@ import SwiftUI
 struct OnboardingView: View {
     // MARK: - PROPERTIES
     
+    var fruits: [Fruit] = fruitsData
+    
     // MARK: - BODY
     var body: some View {
         TabView{
-            ForEach(0..<5) { item in
-                FruitCardView()
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
             }//:LOOP
         }//: TAB
         .tabViewStyle(PageTabViewStyle())
-        .padding(.vertical, 16)
+        .padding(.vertical, 16) 
         
     }
 }
@@ -27,6 +29,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
